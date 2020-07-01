079997cb56bdd455d09425853005dc6f41673b91
BozenaPalicCeric <bozena.palic@gmail.com>
2020-07-01 14:02:35 +0200

    Update Pomocno.java

--- PD_APP/src/main/java/hr/pd_app/util/Pomocno.java
+++ PD_APP/src/main/java/hr/pd_app/util/Pomocno.java
@@ -10,5 +10,38 @@ package hr.pd_app.util;
  * @author Bozena
  */
 public class Pomocno {
-    
+    public static boolean isOibValjan(String oib) {
+
+        if (oib == null) {
+            return false;
+        }
+
+        if (oib.length() != 11) {
+            return false;
+        }
+
+        try {
+            Long.parseLong(oib);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        int a = 10;
+        for (int i = 0; i < 10; i++) {
+            a = a + Integer.parseInt(oib.substring(i, i + 1));
+            a = a % 10;
+            if (a == 0) {
+                a = 10;
+            }
+            a *= 2;
+            a = a % 11;
+        }
+        int kontrolni = 11 - a;
+        if (kontrolni == 10) {
+            kontrolni = 0;
+        }
+
+        return kontrolni == Integer.parseInt(oib.substring(10));
+    }
+
 }
