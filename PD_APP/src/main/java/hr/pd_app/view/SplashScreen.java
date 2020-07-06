f6447dcf33d54beadf070cfdf50eb9b30838916e
BozenaPalicCeric <bozena.palic@gmail.com>
2020-07-06 14:30:33 +0200

    Update SplashScreen.java

--- PD_APP/src/main/java/hr/pd_app/view/SplashScreen.java
+++ PD_APP/src/main/java/hr/pd_app/view/SplashScreen.java
@@ -5,6 +5,9 @@
  */
 package hr.pd_app.view;
 
+import hr.pd_app.util.HibernateUtil;
+import org.hibernate.Session;
+
 /**
  *
  * @author Božena
@@ -16,8 +19,22 @@ public class SplashScreen extends javax.swing.JFrame {
      */
     public SplashScreen() {
         initComponents();
+        Ucitavanje ucitavanje = new Ucitavanje();
+        ucitavanje.start();
     }
 
+    
+    private class Ucitavanje extends Thread {
+
+        @Override
+        public void run() {
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            if (s.getMetamodel().getEntities().size() > 0) {
+                new Autorizacija().setVisible(true);
+                dispose();
+            }
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -58,38 +75,7 @@ public class SplashScreen extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SplashScreen().setVisible(true);
-            }
-        });
-    }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
